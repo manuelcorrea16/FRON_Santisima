3348d92cb090cb5c1602a576f91fca9d5bf8f5cd
Brandon Aguilar <brandonam2017@hotmail.com>
2022-07-18 00:58:48 -0500

    adelanto del proyecto php y js

---
+++ src/js/agendarCita.js
@@ -0,0 +1,17 @@
+form = document.getElementById("form")
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const formData= new FormData(form) 
+    /*const path = ('/profesor/create/create.php')*/
+    const path = ("../../src/php/agendarCita.php")
+    fetch('/ajax/profesor/create/create1.php', {
+        method: 'POST',
+        body: formData,
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        } 
+    }).catch(()=>{
+        alert("no esta funcionando")
+    })
+})
--- src/php/tablas.php
+++ src/php/tablas.php
@@ -2,8 +2,11 @@
     $conn=NULL;
     try{
         $conn = new PDO("mysql:host=localhost; dbname=prueba_bd; charset=utf8", 'root', '');
-		$sql="SELECT * FROM manicurista";
 		
+		if(!empty($_POST)){
+            $tabla = $_POST['table'];
+        }
+        $sql="SELECT * FROM $tabla";
         $stm = $conn->prepare($sql);
         $stm->execute();
         $resultSet = $stm->fetchAll(PDO::FETCH_ASSOC);
